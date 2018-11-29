7176ea7d65bd759fba26ee0a256af938546e5cb4
Kevin Garza <kevin_4569@msn.com>
2018-11-29 10:47:35 -0600

    put2

--- api/usuarios.php
+++ api/usuarios.php
@@ -20,38 +20,26 @@ $app->post('/Usuarios', function ($request, $response, $args) {
    $pass = $postBody->pass;
     
    $db = new DB();
-   $usuario = $db->query('INSERT INTO usuario (UsuarioNombre, UsuarioPass) 
-   							VALUES (:nombre,:pass)', array(':nombre'=>$nombre, ':pass'=>$pass));
-   
-   $usuario = $db->query("SELECT idUsuario, UsuarioNombre, UsuarioPass FROM usuario");
-   return $response->withJson($usuario);
+   $usuario = $db->query('INSERT INTO usuario (UsuarioNombre, UsuarioPass) VALUES (:nombre,:pass)', array(':nombre'=>$nombre, ':pass'=>$pass));
+   return $response->withJson('Usuario insertado correctamente');
 });
 
+$app->put('/Usuarios/{id}', function ($request, $response, $args) {
+   $postBody = file_get_contents("php://input");
+   $postBody = json_decode($postBody);
+   $nombre = $postBody->Nombre;
+   $pass = $postBody->pass;
+
+   $db = new DB();
+   $id = $args['id'];
+   $usuario = $db->query('UPDATE usuario SET UsuarioNombre = :nombre, UsuarioPass = :pass WHERE idUsuario = :id', 
+   						  array(':nombre'=>$nombre, ':pass'=>$pass, ':id'=>$id));
+   return $response->withJson('Usuario modificado correctamente');
+});
 
 $app->delete('/Usuarios/{id}', function ($request, $response, $args) {
    $db = new DB();
    $id = $args['id'];
    $usuario = $db->query('DELETE FROM usuario WHERE idUsuario=:id', array(':id'=>$id)); 
-  	return $response->withJson('Usuario Eliminado Correctamente');
+   return $response->withJson('Usuario eliminado correctamente');
 });
-
-
-/*$app->put('/Usuarios/{id}', function ($request, $response, $args) {
- try{
-   $postBody = file_get_contents("php://input");
-   $postBody = json_decode($postBody);
-   $nombre = $postBody->Nombre;
-   $pass = $postBody->pass;
-    
-   $db = new DB();
-   $id = $args['id'];
-   $usuario = $db->query('UPDATE usuario 
-   						  SET  UsuarioNombre = :nombre,
-   						  WHERE idUsuario = :id', 
-   						  array(':nombre'=>$nombre, ':id'=>$id);
-  } catch (Exception $e){
-  	 return $response->withJson($e->getMessage());
-  } 
-   // $usuario = $db->query("SELECT UsuarioNombre, UsuarioPass FROM usuario WHERE idUsuario = :id'", array(':id'=>$id);
-   // return $response->withJson($usuario);
-});*/
