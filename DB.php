fc8b646c924cbefa5cbe95df3d16f9628a3630bf
Kevin Garza <kevin_4569@msn.com>
2019-06-05 18:18:40 -0500

    Update DB.php

--- DB.php
+++ DB.php
@@ -5,7 +5,7 @@ class DB {
                 $host = "us-cdbr-iron-east-01.cleardb.net";
                 $dbname = "heroku_7bb338c5d45ddfd";
                 $username = "ba50e2945cf767";
-                $password = "5e7f2066";
+                $password = "a4657d73";
                 $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $username, $password);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->pdo = $pdo;
